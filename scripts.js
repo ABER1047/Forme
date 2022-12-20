ad5eeaf50fc070fb651f88ad48ce15cb4fedc178
ABER1047 <78880883+ABER1047@users.noreply.github.com>
2022-12-21 01:43:55 +0900

    2022_12_21
    added new functions

--- scripts.js
+++ scripts.js
@@ -109,6 +109,14 @@ var random_value______ = Math.floor(Math.random()*100 | 1);
     }
 }
 
+//return choosed values randomly
+function choose(val0, /*val1, val2, …, max_val*/)
+{
+var random_choosed__ = irandom_range(0,arguments.length);
+//console.log(arguments[random_choosed__]);
+return arguments[random_choosed__];
+}
+
 //checking that these two circle met
 function place_meeting_circle(x1,y1,x2,y2,rad1,rad2)
 {
@@ -133,4 +141,116 @@ function place_meeting_point(x1,y1,x2,y2,rad)
     {
     return false;
     }
+}
+
+//sign without_zero
+function sign_without(value)
+{
+var _____var = Math.sign(value);
+    if (_____var == 0)
+    {
+    _____var = choose(-1,1);
+    }
+
+return Math.sign(value);
+}
+
+//correct value
+function correct_value(value,min,max)
+{
+var corrected_value__ = value;
+    if (value < min)
+    {
+    corrected_value__ = min;
+    }
+    
+    if (value > max)
+    {
+    corrected_value__ = max;
+    }
+return corrected_value__;
+}
+
+
+//merge color
+function merge_color(color_value1, color_value2, amount/*, debug_mode*/)
+{
+debug_mode = debug_mode || 0;
+
+    if (amount > 1)
+    {
+    amount = 1;
+    }
+    
+    if (amount < 0)
+    {
+    amount = 0;
+    }
+
+    var length_color1 = color_value1.length;
+    var length_color2 = color_value2.length;
+    if (length_color1 == 4)
+    {
+        for(var i = 1; i <= 4; i++)
+        {
+        color_value1 += color_value1[i]+color_value1[i];
+        }
+    }
+    else
+    {
+    color_value1 = color_value1.substring(1);
+    }
+    
+    if (length_color2 == 4)
+    {
+        for(var i = 1; i <= 4; i++)
+        {
+        color_value2 += color_value2[i]+color_value2[i];
+        }
+    }
+    else
+    {
+    color_value2 = color_value2.substring(1);
+    }
+
+
+color_value1 = [parseInt(color_value1[0] + color_value1[1], 16), parseInt(color_value1[2] + color_value1[3], 16), parseInt(color_value1[4] + color_value1[5], 16)];
+color_value2 = [parseInt(color_value2[0] + color_value2[1], 16), parseInt(color_value2[2] + color_value2[3], 16), parseInt(color_value2[4] + color_value2[5], 16)];
+
+var total_color = [(1 - amount) * color_value1[0] + amount * color_value2[0], (1 - amount) * color_value1[1] + amount * color_value2[1], (1 - amount) * color_value1[2] + amount * color_value2[2]];
+total_color = "#" + int_to_hex(total_color[0]) + int_to_hex(total_color[1]) + int_to_hex(total_color[2]);
+
+
+
+if (debug_mode == 1)
+{
+//color output to canvas
+var canvas_ = document.createElement("canvas");
+var ctx = canvas_.getContext("2d");
+canvas_.width = 128;
+canvas_.height = 128;
+document.body.appendChild(canvas_);
+
+ctx.fillStyle = total_color;
+ctx.fillRect(0, 0, 128, 128);
+}
+
+
+return total_color;
+}
+
+function int_to_hex(num)
+{
+var hex = Math.round(num).toString(16);
+    if (hex.length == 1)
+    {
+    hex = '0' + hex;
+    }
+return hex;
+}
+
+//= console.log()
+function debug_log(value)
+{
+console.log(value);
 }
